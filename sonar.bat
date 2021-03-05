307791f03ff6181432c4eb35aa20374e47ea3263
José Antonio Parejo Maestre <japarejo@gmail.com>
2021-03-05 10:01:56 +0100

    Improvements in pom.xml and sonar.bat

--- sonar.bat
+++ sonar.bat
@@ -1,2 +1,2 @@
 setx "SONAR_TOKEN" "2be91a5d51b2d51e76c89403287ce523c0ba8c32"
-mvn -fn verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar
+mvn -fn verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -Dmaven.test.failure.ignore=true -Djacoco.haltonfailure=false
